b70bb0fee4d4fa90cf574883e35796f4904d8fad
seitamuro <seitamuro0210@gmail.com>
2025-02-16 23:19:20 +0900

    2進数を10進数に変換するロジックを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+const binaryToDecimal = (binary: string) => {
+  let ans = 0
+  let cnt = 0;
+  for (const char of Array.from(binary).reverse()) {
+    if (char === "1") {
+      ans += Math.pow(2, cnt)
+    }
+    cnt += 1
+  }
+
+  return ans
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [binary, setBinary] = useState('')
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" onChange={(e) => setBinary(e.target.value)} />
+      <div>{binaryToDecimal(binary)}</div>
     </>
   )
 }
