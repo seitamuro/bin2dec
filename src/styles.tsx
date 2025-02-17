b521bea9bd8b315cb71e82d3f0fd63eb73cd8561
seitamuro <seitamuro0210@gmail.com>
2025-02-18 00:06:58 +0900

    不要なstyled-componentsを削除

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,22 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 1rem;
-`
 export const Digit8Container = styled.div`
   display: grid;
   grid-template-columns: repeat(8, 0);
   gap: 4rem;
   margin: 10px;
-`
-
-export const Label = styled.span`
-  margin-right: 1rem;
-  width: 80px;
-  text-align: right;
-  font-size: 18px;
 `
 
 export const BinaryDigitTextInput = styled.input`
@@ -35,21 +23,8 @@ export const BinaryDigitTextInput = styled.input`
   }
 `
 
-export const BinaryTextInput = styled.input`
-  width: 50%;
-  font-size: 18px;
-  border: 1px solid #a3a3a3;
-  padding: 10px;
-  border-radius: 2px;
-  outline: none;
-
-  &:focus {
-    border: 1px solid orange;
-  }
-`
-
 export const DecimalTextOutput = styled.input`
-  width: 50%;
+  width 100px;
   font-size: 18px;
   border: 1px solid #a3a3a3;
   padding: 10px;
